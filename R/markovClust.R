3807d9a8cf192b9f70960c22d389f8dc2bf88e29
tomek <tomis9@buziaczek.pl>
2017-09-18 10:27:10 +0200

    class MarkovClustObject, summary method

--- R/markovClust.R
+++ R/markovClust.R
@@ -6,7 +6,7 @@
 #' @export
 #' @examples
 
-markovClust <- function(M, r, eStop) {
+markovClust <- function(M, r=2.5, eStop=0.001) {
     inflation <- function(M, r) return(M ^ r / rowSums(M ^ r))
     frobeniusNorm <- function(M1, M0) return(sqrt(sum((M1 - M0) ^ 2)))
     for (i in 1:100) {
@@ -15,6 +15,7 @@ markovClust <- function(M, r, eStop) {
         M <- inflation(M, r)
         if (frobeniusNorm(M, Mt1) < 0.001) break
     }
-    return(round(M))
+    M <- structure(round(M), class = c("MarkovClustObject", "matrix"))
+    return(M)
 }
 
---
+++ R/summary.MarkovClustObject.R
@@ -0,0 +1,13 @@
+#' Summarizing Markov clustering algorithm
+#'
+#' Markov clustering function algorithm
+#' @param 
+#' @keywords 
+#' @export
+#' @examples
+
+summary.MarkovClustObject <- function(M) {
+    list(title="Markov clustering algorithm results",
+         M=M)
+}
+
