3b84033d2d19d33a608cbfd4096ee955b7b92588
tomek <tomis9@buziaczek.pl>
2017-09-18 11:23:44 +0200

    simple print method

---
+++ R/print.MarkovClustObject.R
@@ -0,0 +1,14 @@
+#' Summarizing Markov clustering algorithm
+#'
+#' Summarizing Markov clustering algorithm
+#' @param 
+#' @keywords 
+#' @export
+#' @examples
+
+print.MarkovClustObject <- function(MCO) {
+    cat("Markov clustering algorithm\n\n")
+    cat("nLoops: ", MCO$nLoops, "\n")
+}
+
+
