4ed2919e8842643f29865ce229383c8b47df3108
tomek <tomis9@buziaczek.pl>
2017-09-20 15:12:33 +0200

    print and summary parameter definitions

--- R/print.MarkovClustObject.R
+++ R/print.MarkovClustObject.R
@@ -1,15 +1,22 @@
-#' Summarizing Markov clustering algorithm
+#' Print Markov clustering algorithm
 #'
-#' Summarizing Markov clustering algorithm
-#' @param 
-#' @keywords 
+#' Print Markov clustering algorithm
+#' @param MCO element of class MarkovClustObject
+#' @keywords markov clustering, graph clustering
 #' @export
 #' @examples
+#' A <- rbind(c(1, 1, 0, 1, 0, 1, 0),
+#'            c(1, 1, 1, 1, 0, 0, 0),
+#'            c(0, 1, 1, 1, 0, 0, 1),
+#'            c(1, 1, 1, 1, 1, 0, 0),
+#'            c(0, 0, 0, 1, 1, 1, 1),
+#'            c(1, 0, 0, 0, 1, 1, 1),
+#'            c(0, 0, 1, 0, 1, 1, 1))
+#' markovClust(A)
 
 print.MarkovClustObject <- function(MCO) {
     cat("Markov clustering algorithm\n\n")
     cat("nLoops: ", MCO$nLoops, "\n\n")
     cat("order: ", MCO$ord, "\n")
 }
-
 
--- R/summary.MarkovClustObject.R
+++ R/summary.MarkovClustObject.R
@@ -1,10 +1,19 @@
 #' Summarizing Markov clustering algorithm
 #'
 #' Summarizing Markov clustering algorithm
-#' @param 
-#' @keywords 
+#' @param MCO element of class MarkovClustObject
+#' @keywords markov clustering, graph clustering
 #' @export
 #' @examples
+#' A <- rbind(c(1, 1, 0, 1, 0, 1, 0),
+#'            c(1, 1, 1, 1, 0, 0, 0),
+#'            c(0, 1, 1, 1, 0, 0, 1),
+#'            c(1, 1, 1, 1, 1, 0, 0),
+#'            c(0, 0, 0, 1, 1, 1, 1),
+#'            c(1, 0, 0, 0, 1, 1, 1),
+#'            c(0, 0, 1, 0, 1, 1, 1))
+#' m <- markovClust(A)
+#' summary(m)
 
 summary.MarkovClustObject <- function(MCO) {
     list(title="Markov clustering algorithm results",
