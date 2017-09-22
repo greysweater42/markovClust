791e3977f9435927a1ba9383596c6fb53b8fa374
tomek <tomis9@buziaczek.pl>
2017-09-23 00:00:44 +0200

    k nearest neighbours in kernel weights

--- R/kernel_weights.R
+++ R/kernel_weights.R
@@ -1,13 +1,15 @@
 #' Gaussian kernel weights
 #'
 #' Gaussian kernel weighhts
-#' @param 
+#' @param d data
+#' @param phi some parameter, usually 0.5
+#' @param k k newarest neighbours
 #' @keywords markov clustering, graph clustering
 #' @useDynLib markovClust
 #' @export
 #' @examples 
 
-kernel_weights <- function(d, phi=0.5) {
+kernel_weights <- function(d, phi=0.5, k) {
     X <- t(as.matrix(d))
     storage.mode(X) <- "double"
     p <- as.integer(nrow(X))
@@ -18,6 +20,13 @@ kernel_weights <- function(d, phi=0.5) {
     A <- matrix(0, n, n)
     A[lower.tri(A)] <- sol$w
     A <- A + t(A) + diag(n)
+    if (!missing(k)) {
+        for (i in 1:nrow(A)) {
+            threshold <- sort(A[i,], decreasing=T)[k+1]
+            A[i, A[i,] < threshold] <- 0
+        }
+        A[!A == t(A)] <- 0
+    }
     return(A)
 }
 
